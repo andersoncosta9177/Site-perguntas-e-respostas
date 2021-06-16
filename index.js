5e54856c30002e5ed8cc8eafc4fa1de4151b7b5e
Anderson Costa <anderson_costa_miguel@hotmail.com>
2021-06-16 15:19:01 -0300

    site de perguntas e respostas

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require("express")
+const  app = express()
+const bodyParser = require("body-parser")
+const connection = require('./database/database')
+const pergunta = require('./database/pergunta')
+const Pergunta = require("./database/pergunta")
+const Resposta = require("./database/Resposta")
+
+// database
+
+connection
+ .authenticate()
+ .then(()=>{
+     console.log("conectado com sucesso!")
+ })
+ .catch((msgErro)=>{
+     
+     console.log(msgErro)
+ })
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+app.get("/",(req,res)=>{
+    Pergunta.findAll({raw: true,order:[
+        ['id','desc']
+    ]}).then(pergunta=>{
+        res.render("index",{
+            pergunta: pergunta
+        })
+    })
+})
+
+app.get("/perguntar",(req,res)=>{
+    res.render('perguntar');
+})
+
+
+
+app.post("/salvarPergunta",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(()=>{
+        res.redirect("/")
+    })
+ 
+}) 
+
+
+app.get("/pergunta/:id",(req,res)=>{
+   var id = req.params.id
+   Pergunta.findOne({
+       where:{id:id},
+     
+   }).then(pergunta => {
+       if(pergunta != undefined){
+
+    
+        Resposta.findAll({
+            where:{ perguntaId:pergunta.id},
+            order: [['id','desc']]
+        }).then(respostas =>{
+            res.render("pergunta",{
+                pergunta: pergunta,
+                respostas : respostas
+            })
+        }) 
+            
+       }else{
+          res.redirect("/")
+       }
+   })
+})
+
+
+
+app.post("/responder",(req,res)=>{
+    var corpo = req.body.corpo;
+    var perguntaId =    req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+          perguntaId: perguntaId
+    }).then(()=>{
+        res.redirect("/pergunta/"+perguntaId)
+    })
+
+})
+
+
+app.listen(8086,()=>{
+    console.log("rodando")
+})
+
+
